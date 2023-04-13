576a720c9fb90f94ab4fa16c95f451cdc1ea75a3
Aleksandr <aleksandr.tokhiyan@gmail.com>
2023-04-13 18:38:44 +0400

    change api key

--- src/constans.js
+++ src/constans.js
@@ -1,3 +1,3 @@
 export const API_URL = "https://dataservice.accuweather.com";
-export const AcuWeather_api_key = "kadoHUXbvA7kTc1RKDT1VW5gWdjF4EY1";
+export const AcuWeather_api_key = "SsVWnvAis6IuzN6nsPp3wjcVA6GNOXok";
 export const IpGeolocation_api_key = "74be3fa33c6d4855a47648e55e58ff8f";
